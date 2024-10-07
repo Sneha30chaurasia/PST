251596d5214288ebfbfe3527bfd3f788530940c4
Sneha Chaurasia <chaurasiasneha30@gmail.com>
2024-10-07 20:50:43 +0530

    Delete ASSIGMENT.JAVA SCRIPT directory

--- ASSIGMENT.JAVA SCRIPT/operator assin.js
+++
@@ -1,7 +0,0 @@
-function isEligble(hasmem,totalpurchase){
-if (hasmem && totalpurchase>100)
-    {return true;}
-else
- {false;}
-}
-const c1=isEligble(true,100)
--- ASSIGMENT.JAVA SCRIPT/tes1t.js
+++
@@ -1,9 +0,0 @@
-function person(name,age)
-{
-  return{  name,
-    age}
-}
-
-
-let person1=person("sneha",18)
-console.log (person1)
